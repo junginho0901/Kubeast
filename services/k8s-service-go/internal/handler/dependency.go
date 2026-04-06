e21b865c7fb936e80f656205f8803164bfeaa140
JeongInho <cn5114555@naver.com>
2026-04-06 10:45:53 +0900

    feat(k8s-go): add dependency graph HTTP handler

---
+++ services/k8s-service-go/internal/handler/dependency.go
@@ -0,0 +1,20 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/junginho0901/kube-assistant/services/pkg/response"
+)
+
+// GetDependencyGraph handles GET /api/v1/cluster/namespaces/{namespace}/dependency-graph.
+func (h *Handler) GetDependencyGraph(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	namespace := chi.URLParam(r, "namespace")
+	data, err := h.svc.GetDependencyGraph(ctx, namespace)
+	if err != nil {
+		h.handleError(w, err)
+		return
+	}
+	response.JSON(w, http.StatusOK, data)
+}
